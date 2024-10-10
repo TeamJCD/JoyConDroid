dbc621722f38fab70086d7247abd1813e0acae34
X <119499221+Cyphs@users.noreply.github.com>
2024-10-10 02:59:16 -0400

    Update urls for instructions and custom UI

--- app/src/main/java/com/rdapps/gamepad/customui/CustomUiClient.java
+++ app/src/main/java/com/rdapps/gamepad/customui/CustomUiClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
 public class CustomUiClient {
-    private static final String BASE_URL = "http://joycondroid.youtubeplays.com";
+    private static final String BASE_URL = "https://youtubeplays.github.io/JoyConDroidJS/";
 
     private static Retrofit retrofit = null;
     private static CustomUiService service = null;
--- app/src/main/java/com/rdapps/gamepad/customui/CustomUiService.java
+++ app/src/main/java/com/rdapps/gamepad/customui/CustomUiService.java
@@ -7,6 +7,6 @@ import retrofit2.http.GET;
 
 public interface CustomUiService {
 
-    @GET("/")
+    @GET("index.html")
     Call<List<CustomUiItem>> getCustomUis();
 }
